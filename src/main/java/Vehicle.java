5e152d9696259ccdfc9c8c0a5802ff7d83c68400
Paul Smith <paul.smith@wolterskluwer.com>
2018-01-31 15:57:51 -0500

    ZZ#1 - PS - Add control methods.

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -17,4 +17,16 @@ public final class Vehicle {
     public Long getCurrentDirection() {
         return 0L;
     }
+
+    //Method to add direction to the current direction
+    void steer(int direction) {
+    }
+
+    //Method to set velocity to currentVelocity and direction to currentDirection.
+    void move(int velocity, int direction) {
+    }
+
+    //Method to change the currentVelocity to zero
+    void stop() {
+    }
 }
